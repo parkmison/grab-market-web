bf5f8e1c26a4360e76910eafd414b9bb6843da29
parkmiso <nomiso23@gmail.com>
2022-01-15 00:48:48 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ChildComponent from "./child.js";
 
 function App() {
+  const text = "안녕하세요 미박소입니다";
+  const sayHello = function () {
+    return <h3> 인프런 강의 너무 조아</h3>;
+  };
+  const sayHello2 = function () {
+    alert("ㅎㅇ");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> hello!! </h1>
+      <h2>{text}</h2>
+      {sayHello()}
+      <div
+        onClick={function () {
+          alert("ㅎㅇ");
+        }}
+      >
+        클릭점
+      </div>
+      <ChildComponent name="그랩" age={27} />
+      <ChildComponent name="미소" age={26} />
+      <ChildComponent name="gg" age={50} />
+      <ChildComponent name="hi" age={34} />
     </div>
   );
 }
