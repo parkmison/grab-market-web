a13d4a7c01800db70a5c0e4a061bbe29352c02c5
parkmiso <nomiso23@gmail.com>
2022-03-25 18:54:48 +0900

    master

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,36 +1,52 @@
-import { toBeRequired } from "@testing-library/jest-dom/dist/matchers";
-import {
-  Divider,
-  Form,
-  Input,
-  required,
-  message,
-  InputNumber,
-  Button,
-} from "antd";
+import { Divider, Form, Input, InputNumber, Button, Upload } from "antd";
 import "./index.css";
+import { ForkOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 function UploadPage() {
+  const [imageUrl, setImageUrl] = useState(null);
   const onSubmit = (values) => {
     console.log(values);
   };
+  const onChangeImage = (info) => {
+    if (info.file.status === "uploading") {
+      return;
+    }
+    if (info.file.status === "done") {
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
+  };
   return (
     <div>
-      <Form name="상품 업로드" onFinish={onsubmit}>
+      <Form name="상품 업로드" onFinish={onSubmit}>
         <Form.Item
           name="upload"
           label={<div className="upload-label">상품 사진</div>}
         >
-          <div id="upload-img-placeholder">
-            <img src="/images/icons/camera.png" />
-            <span>이미지를 업로드해주세요.</span>
-          </div>
+          <Upload
+            name="image"
+            action="http://localhost:8082/image"
+            listType="picture"
+            showUploadList={false}
+            onChange={onChangeImage}
+          >
+            {imageUrl ? (
+              <img id="upload-img" src={`http://localhost:8082/${imageUrl}`} />
+            ) : (
+              <div id="upload-img-placeholder">
+                <img src="/images/icons/camera.png" />
+                <span>이미지를 업로드해주세요.</span>
+              </div>
+            )}
+          </Upload>
         </Form.Item>
         <Divider />
         <Form.Item
           label={<div className="upload-label">판매자 명</div>}
           name="seller"
-          rules={[{ required: true, message: "판매자명을 입력해주세요." }]}
+          rules={[{ required: true, message: "판매자 이름을 입력해주세요" }]}
         >
           <Input
             className="upload-name"
