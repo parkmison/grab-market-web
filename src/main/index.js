bffda97f72e7226ec3e8780008194b2e3a3beebc
parkmiso <nomiso23@gmail.com>
2022-04-11 20:30:40 +0900

    master

--- src/main/index.js
+++ src/main/index.js
@@ -5,11 +5,13 @@ import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { API_URL } from "../config/constants.js";
+import { Carousel } from "antd";
 
 dayjs.extend(relativeTime);
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
       .get(`${API_URL}/products`)
@@ -20,18 +22,37 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러가 발생했습니다 :", error);
       });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
+      });
   }, []);
 
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" alt="zz" />
-      </div>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
       <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link
                 style={{ color: "inherit" }}
                 className="product-link"
@@ -45,7 +66,7 @@ function MainPage() {
                 </div>
                 <div className="product-contents">
                   <span className="product-name">{product.name}</span>
-                  <span className="product-price">{product.price}</span>
+                  <span className="product-price">{product.price}원</span>
                   <div className="product-footer">
                     <div className="product-seller">
                       <img
