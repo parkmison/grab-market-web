c801de7931c3f8912477dbeff455ec949b0b1ef2
parkmiso <nomiso23@gmail.com>
2022-04-11 21:46:08 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8082"; //export여러개하려면 const
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://pms-grab-market.herokuapp.com" /*클라우드에선 이거, 로컬호스트에선 아래*/
+    : "http://localhost:8082"; //export여러개하려면 const
