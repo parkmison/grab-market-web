717879b14b0a3f10e70e46a09f6c01549d39a487
parkmiso <nomiso23@gmail.com>
2022-04-12 17:17:52 +0900

    API URL change

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://pms-grab-market.herokuapp.com" /*클라우드에선 이거, 로컬호스트에선 아래*/
-    : "http://localhost:8082"; //export여러개하려면 const
+export const API_URL = "https://pms-grab-market.herokuapp.com";
+/* process.env.NODE_ENV === "production"
+    ? "https://pms-grab-market.herokuapp.com" 클라우드에선 이거, 로컬호스트에선 아래
+    : "http://localhost:8082"; export여러개하려면 const*/
