768fab85c9e6205c47ca2e1aba2c543b14762e41
parkmiso <nomiso23@gmail.com>
2022-04-19 22:18:23 +0900

    진짜 제가 다시는 이거안하고싶어요 앱개발 너무힘들어요 미래의 미소군은 이거 다까먹었겟지 그래 이해한다 짜식 파이팅

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
+export const API_URL = "http://localhost:8082";
+/*process.env.NODE_ENV === "production"
     ? "https://pms-grab-market.herokuapp.com"
-    : "http://localhost:8082";
+    : "http://localhost:8082";*/
